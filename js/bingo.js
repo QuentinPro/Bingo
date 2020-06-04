23eac40e4b7f3ac16736c7995e41f38e727aa524
Quentin <qloiseau.pro@outlook.fr>
2020-06-04 11:51:23 +0200

    Début des conditions de victoire

--- js/bingo.js
+++ js/bingo.js
@@ -1,3 +1,5 @@
+$(".hidden").hide();
+
 $("td").on("click", function(){
 	
 	$(this).toggleClass("clicked");
@@ -5,4 +7,16 @@ $("td").on("click", function(){
 
 $(".reset").click(function() {
 	$("td").removeClass("clicked");
+	$(".hidden").hide();
 });
+
+
+function show(){
+    if ($(".h-1", this) && $(".h-2", this) && $(".h-3", this) && $(".h-4", this).hasClass('clicked')){
+        $(".hidden").show();
+        // alert("Félicitations, vous avez remporté le bingo de la toxicité pour cette partie! (malheureusement)");
+    }
+}
+
+// or, simplifying further
+$("#first_lign, #second_lign, #third_lign, #fourth_lign").click(show);
